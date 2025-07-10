351a81789159ff857539ed2713d892345ab18181
Vishwajeet Kumar Sudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2025-07-10 08:43:27 +0530

    Delete Flow Controls Statements/examp1.py

--- Flow Controls Statements/examp1.py
+++
@@ -1,6 +0,0 @@
-tkt=input("Do u have a Ticket(yes/no): ")
-if(tkt.lower()=="yes"):
-    print("\tEnter into theater")
-    print("\tEnjoy the movie")
-    print("\tUnderstand the message of movie")
-print("Go to Home")
