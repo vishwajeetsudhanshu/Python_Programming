bfa2d3a91323e7d56ab56bbb8217fbb4ef72b59f
Vishwajeet Kumar Sudhanshu <124762061+vishwajeetsudhanshu@users.noreply.github.com>
2025-07-10 08:43:42 +0530

    Delete Flow Controls Statements/examp2.py

--- Flow Controls Statements/examp2.py
+++
@@ -1,8 +0,0 @@
-value=float(input("Enter any value: "))
-if(value>0):
-    print("{} is a +ve Number".format(value))
-if(value<0):
-    print("{} is -ve Number".format(value))
-if(value==0):
-    print("{} is Zero".format(value))
-print("Program executed Succesfully")
